7d96f7b2656505f380c2cfae8c649fc44254ee8b
Trevor McDonell <trevor.mcdonell@gmail.com>
2009-07-17 13:45:08 +1000

    remove old c stubs, offsets now handled by the preprocessor
    Ignore-this: 3e19a707cf781f0152a5fa72eb2dcf7d
    
    darcs-hash:20090717034508-115f9-3911399aec4520003df0a85c3c75b78f5c5fb942.gz

--- cbits/stubs.c
+++ cbits/stubs.c
@@ -4,24 +4,4 @@
 
 #include "stubs.h"
 
-/*
- * DeviceProperties
- *
- * Required because the C->Haskell get hook deferences static arrays but still
- * treats them as pointer type.
- */
-int offset_devName()
-{
-    return offsetof(struct cudaDeviceProp, name);
-}
-
-int offset_devMaxThreadDim()
-{
-    return offsetof(struct cudaDeviceProp, maxThreadsDim);
-}
-
-int offset_devMaxGridSize()
-{
-    return offsetof(struct cudaDeviceProp, maxGridSize);
-}
 
