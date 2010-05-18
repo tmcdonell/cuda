6e62f6ae582822ee498fe0af325742d46882336a
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2010-05-19 09:58:52 +1000

    meta-module for analysis routines
    Ignore-this: 64b338db23cc8fc180628472301897cc
    
    darcs-hash:20100518235852-dcabc-65c5355ad7dc99b864dda11705aa552a1c8a6f7f.gz

---
+++ Foreign/CUDA/Analysis.hs
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module    : Foreign.CUDA.Analysis
+-- Copyright : (c) [2009..2010] Trevor L. McDonell
+-- License   : BSD
+--
+-- Meta-module exporting CUDA analysis routines
+--
+--------------------------------------------------------------------------------
+
+module Foreign.CUDA.Analysis
+  (
+    module Foreign.CUDA.Analysis.Device,
+    module Foreign.CUDA.Analysis.Occupancy
+  )
+  where
+
+import Foreign.CUDA.Analysis.Device
+import Foreign.CUDA.Analysis.Occupancy
+
