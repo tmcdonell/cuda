b5918fadc48d08a8c7f3fb8c26e6fe350a645a1c
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2010-07-05 23:39:50 +1000

    automatically disable Blocks extension on snow leopard
    Ignore-this: 207bb3d70fc228cc0c2ec1f59d8951d9
    
    darcs-hash:20100705133950-dcabc-875afd65868a38aa24d925f161c40bfacb735171.gz

--- Setup.hs
+++ Setup.hs
@@ -1,4 +1,71 @@
 import Distribution.Simple
+import Distribution.Simple.Program
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.PreProcess hiding (ppC2hs)
+import Distribution.PackageDescription
+import System.FilePath
+
 
 main :: IO ()
-main =  defaultMain
+main = defaultMainWithHooks customHooks
+  where
+    preprocessors = hookedPreProcessors autoconfUserHooks
+    customHooks   = autoconfUserHooks {
+      hookedPreProcessors = ("chs",ppC2hs) : filter (\x -> fst x /= "chs") preprocessors
+    }
+
+-- Replicate the default C2HS preprocessor hook here, and inject a value for
+-- extra-c2hs-options, if it was present in the buildinfo file
+--
+-- Everything below copied from Distribution.Simple.PreProcess
+--
+ppC2hs :: BuildInfo -> LocalBuildInfo -> PreProcessor
+ppC2hs bi lbi
+    = PreProcessor {
+        platformIndependent = False,
+        runPreProcessor = \(inBaseDir, inRelativeFile)
+                           (outBaseDir, outRelativeFile) verbosity ->
+          rawSystemProgramConf verbosity c2hsProgram (withPrograms lbi) $
+            extra_flags
+            ++ ["--include=" ++ outBaseDir]
+            ++ ["--cppopts=" ++ opt | opt <- getCppOptions bi lbi]
+            ++ ["--output-dir=" ++ outBaseDir,
+                "--output=" ++ outRelativeFile,
+                inBaseDir </> inRelativeFile]
+      }
+  where
+    extra_flags = case lookup "x-extra-c2hs-options" (customFieldsBI bi) of
+                       Nothing -> []
+                       Just s  -> [s]
+
+getCppOptions :: BuildInfo -> LocalBuildInfo -> [String]
+getCppOptions bi lbi
+    = hcDefines (compiler lbi)
+   ++ ["-I" ++ dir | dir <- includeDirs bi]
+   ++ [opt | opt@('-':c:_) <- ccOptions bi, c `elem` "DIU"]
+
+hcDefines :: Compiler -> [String]
+hcDefines comp =
+  case compilerFlavor comp of
+    GHC  -> ["-D__GLASGOW_HASKELL__=" ++ versionInt version]
+    JHC  -> ["-D__JHC__=" ++ versionInt version]
+    NHC  -> ["-D__NHC__=" ++ versionInt version]
+    Hugs -> ["-D__HUGS__"]
+    _    -> []
+  where version = compilerVersion comp
+
+-- TODO: move this into the compiler abstraction
+-- FIXME: this forces GHC's crazy 4.8.2 -> 408 convention on all the other
+-- compilers. Check if that's really what they want.
+versionInt :: Version -> String
+versionInt (Version { versionBranch = [] }) = "1"
+versionInt (Version { versionBranch = [n] }) = show n
+versionInt (Version { versionBranch = n1:n2:_ })
+  = -- 6.8.x -> 608
+    -- 6.10.x -> 610
+    let s1 = show n1
+        s2 = show n2
+        middle = case s2 of
+                 _ : _ : _ -> ""
+                 _         -> "0"
+    in s1 ++ middle ++ s2
