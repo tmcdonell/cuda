57063b15912d667f5f27b81db176347b90f83cfc
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2012-09-10 11:08:04 +1000

    inline some conversion functions

--- Foreign/CUDA/Internal/C2HS.hs
+++ Foreign/CUDA/Internal/C2HS.hs
@@ -190,35 +190,42 @@ extractBitMasks bits =
 
 -- |Integral conversion
 --
+{-# INLINE cIntConv #-}
 cIntConv :: (Integral a, Integral b) => a -> b
 cIntConv  = fromIntegral
 
 -- |Floating conversion
 --
+{-# INLINE cFloatConv #-}
 cFloatConv :: (RealFloat a, RealFloat b) => a -> b
 cFloatConv  = realToFrac
 -- As this conversion by default goes via `Rational', it can be very slow...
-{-# RULES 
+{-# RULES
   "cFloatConv/Float->Float"   forall (x::Float).  cFloatConv x = x;
   "cFloatConv/Double->Double" forall (x::Double). cFloatConv x = x
  #-}
 
 -- |Obtain C value from Haskell 'Bool'.
 --
+{-# INLINE cFromBool #-}
 cFromBool :: Num a => Bool -> a
 cFromBool  = fromBool
 
 -- |Obtain Haskell 'Bool' from C value.
 --
+{-# INLINE cToBool #-}
 cToBool :: (Eq a, Num a) => a -> Bool
 cToBool  = toBool
 
 -- |Convert a C enumeration to Haskell.
 --
+{-# INLINE cToEnum #-}
 cToEnum :: (Integral i, Enum e) => i -> e
 cToEnum  = toEnum . cIntConv
 
 -- |Convert a Haskell enumeration to C.
 --
+{-# INLINE cFromEnum #-}
 cFromEnum :: (Enum e, Integral i) => e -> i
 cFromEnum  = cIntConv . fromEnum
+
