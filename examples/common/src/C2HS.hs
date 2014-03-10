5c8728725faf2dce25a1c781b0d9c0396bd1eaa5
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-03-10 11:36:25 -0700

    rule wibbles

--- examples/common/src/C2HS.hs
+++ examples/common/src/C2HS.hs
@@ -190,17 +190,23 @@ extractBitMasks bits =
 
 -- |Integral conversion
 --
+{-# INLINE cIntConv #-}
 cIntConv :: (Integral a, Integral b) => a -> b
 cIntConv  = fromIntegral
 
 -- |Floating conversion
 --
+{-# INLINE [1] cFloatConv #-}
 cFloatConv :: (RealFloat a, RealFloat b) => a -> b
 cFloatConv  = realToFrac
 -- As this conversion by default goes via `Rational', it can be very slow...
-{-# RULES 
-  "cFloatConv/Float->Float"   forall (x::Float).  cFloatConv x = x;
-  "cFloatConv/Double->Double" forall (x::Double). cFloatConv x = x
+{-# RULES
+  "cFloatConv/Float->Float"    forall (x::Float).  cFloatConv x = x;
+  "cFloatConv/Double->Double"  forall (x::Double). cFloatConv x = x;
+  "cFloatConv/Float->CFloat"   forall (x::Float).  cFloatConv x = CFloat x;
+  "cFloatConv/CFloat->Float"   forall (x::Float).  cFloatConv CFloat x = x;
+  "cFloatConv/Double->CDouble" forall (x::Double). cFloatConv x = CDouble x;
+  "cFloatConv/CDouble->Double" forall (x::Double). cFloatConv CDouble x = x
  #-}
 
 -- |Obtain C value from Haskell 'Bool'.
