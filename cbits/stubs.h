0c400922be461a9685b0f4b57d0d0860baa74822
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-10-06 10:39:28 -0400

    tabs and whitespace

--- cbits/stubs.h
+++ cbits/stubs.h
@@ -14,11 +14,11 @@
 /*
  * We need to work around some shortcomings in the C parser of c2hs by disabling advanced attributes etc on Apple platforms.
  */
-#ifdef __APPLE__ 
+#ifdef __APPLE__
 #define _ANSI_SOURCE
 #define __AVAILABILITY__
 #define __OSX_AVAILABLE_STARTING(_mac, _iphone)
-#define __OSX_AVAILABLE_BUT_DEPRECATED(_macIntro, _macDep, _iphoneIntro, _iphoneDep) 
+#define __OSX_AVAILABLE_BUT_DEPRECATED(_macIntro, _macDep, _iphoneIntro, _iphoneDep)
 #endif
 
 #include <cuda.h>
