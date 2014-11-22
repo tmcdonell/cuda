42924f3e0189d391cfb3692a4d1fbed68b84282d
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-11-22 17:55:08 -0500

    compilation fix for Yosemite
    tmcdonell/cuda#18

--- cbits/stubs.h
+++ cbits/stubs.h
@@ -19,6 +19,7 @@
 #define __AVAILABILITY__
 #define __OSX_AVAILABLE_STARTING(_mac, _iphone)
 #define __OSX_AVAILABLE_BUT_DEPRECATED(_macIntro, _macDep, _iphoneIntro, _iphoneDep)
+#define __OSX_AVAILABLE_BUT_DEPRECATED_MSG(_osxIntro, _osxDep, _iosIntro, _iosDep, _msg)
 #endif
 
 #include <cuda.h>
