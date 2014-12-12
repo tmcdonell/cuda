698d105ff6c9e64e458a9ce17b5e90a2694b2a85
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-12-12 14:08:59 -0500

    warning police: rule wibbles

--- Foreign/CUDA/Internal/C2HS.hs
+++ Foreign/CUDA/Internal/C2HS.hs
@@ -177,42 +177,46 @@ extractBitMasks bits =
 
 -- |Integral conversion
 --
-{-# INLINE cIntConv #-}
+{-# INLINE [1] cIntConv #-}
 cIntConv :: (Integral a, Integral b) => a -> b
 cIntConv  = fromIntegral
 
 -- |Floating conversion
 --
-{-# INLINE cFloatConv #-}
+{-# INLINE [1] cFloatConv #-}
 cFloatConv :: (RealFloat a, RealFloat b) => a -> b
 cFloatConv  = realToFrac
 -- As this conversion by default goes via `Rational', it can be very slow...
 {-# RULES
-  "cFloatConv/Float->Float"   forall (x::Float).  cFloatConv x = x;
-  "cFloatConv/Double->Double" forall (x::Double). cFloatConv x = x
+  "cFloatConv/Float->Float"    forall (x::Float).  cFloatConv x = x;
+  "cFloatConv/Double->Double"  forall (x::Double). cFloatConv x = x;
+  "cFloatConv/Float->CFloat"   forall (x::Float).  cFloatConv x = CFloat x;
+  "cFloatConv/CFloat->Float"   forall (x::Float).  cFloatConv CFloat x = x;
+  "cFloatConv/Double->CDouble" forall (x::Double). cFloatConv x = CDouble x;
+  "cFloatConv/CDouble->Double" forall (x::Double). cFloatConv CDouble x = x
  #-}
 
 -- |Obtain C value from Haskell 'Bool'.
 --
-{-# INLINE cFromBool #-}
+{-# INLINE [1] cFromBool #-}
 cFromBool :: Num a => Bool -> a
 cFromBool  = fromBool
 
 -- |Obtain Haskell 'Bool' from C value.
 --
-{-# INLINE cToBool #-}
+{-# INLINE [1] cToBool #-}
 cToBool :: (Eq a, Num a) => a -> Bool
 cToBool  = toBool
 
 -- |Convert a C enumeration to Haskell.
 --
-{-# INLINE cToEnum #-}
+{-# INLINE [1] cToEnum #-}
 cToEnum :: (Integral i, Enum e) => i -> e
 cToEnum  = toEnum . cIntConv
 
 -- |Convert a Haskell enumeration to C.
 --
-{-# INLINE cFromEnum #-}
+{-# INLINE [1] cFromEnum #-}
 cFromEnum :: (Enum e, Integral i) => e -> i
 cFromEnum  = cIntConv . fromEnum
 
