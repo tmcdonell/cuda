f1ce160f7f85533f274feacd63d550bfd62492e2
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-10-21 11:13:34 +1100

    Build fix for El Capitan
    Availability macros on OS X cause problems with c2hs, but it appears this has been fixed as of haskell/c2hs#85 and released as part of c2hs-0.19.1, so we don't need this block anymore.

--- cbits/stubs.h
+++ cbits/stubs.h
@@ -11,19 +11,6 @@
 #define CUDARTAPI __stdcall
 #endif
 
-// #define __cdecl
-
-/*
- * We need to work around some shortcomings in the C parser of c2hs by disabling advanced attributes etc on Apple platforms.
- */
-#ifdef __APPLE__
-#define _ANSI_SOURCE
-#define __AVAILABILITY__
-#define __OSX_AVAILABLE_STARTING(_mac, _iphone)
-#define __OSX_AVAILABLE_BUT_DEPRECATED(_macIntro, _macDep, _iphoneIntro, _iphoneDep)
-#define __OSX_AVAILABLE_BUT_DEPRECATED_MSG(_osxIntro, _osxDep, _iosIntro, _iosDep, _msg)
-#endif
-
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 
