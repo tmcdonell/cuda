8e366603a99635bb28ce1ee54dd029b4e59c3d1f
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-05-27 20:12:35 +1000

    Add operations for unified memory addressing

--- src/Foreign/CUDA/Driver.hs
+++ src/Foreign/CUDA/Driver.hs
@@ -223,6 +223,7 @@ module Foreign.CUDA.Driver (
   module Foreign.CUDA.Driver.Exec,
   module Foreign.CUDA.Driver.Marshal,
   module Foreign.CUDA.Driver.Module,
+  module Foreign.CUDA.Driver.Unified,
   module Foreign.CUDA.Driver.Utils
 
 ) where
@@ -234,5 +235,6 @@ import Foreign.CUDA.Driver.Error
 import Foreign.CUDA.Driver.Exec
 import Foreign.CUDA.Driver.Marshal      hiding ( useDeviceHandle, peekDeviceHandle )
 import Foreign.CUDA.Driver.Module
+import Foreign.CUDA.Driver.Unified
 import Foreign.CUDA.Driver.Utils
 
